22a655307d5108fe15ad3044bbb7b28af17f92d3
Faithfulness Alamu <vaguemail369@gmail.com>
2021-04-10 19:15:53 +0100

    Add function to retrieve launch port

---
+++ internal/pkg/web/env.go
@@ -0,0 +1,19 @@
+package web
+
+import "os"
+
+// Server environment variables
+const (
+	// M2W_PORT variable determines the port to start the server on
+	M2W_PORT = "M2W_PORT"
+)
+
+// getPort returns the M2W_PORT environment variable.
+// For compatibility with some platforms (e.g. Heroku), it falls back to PORT if M2W_PORT is empty.
+func getPort() string {
+	port, ok := os.LookupEnv(M2W_PORT)
+	if !ok {
+		port = os.Getenv("PORT")
+	}
+	return port
+}
