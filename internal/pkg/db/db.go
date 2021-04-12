9b638e0041f5836e355ca233fcb47426202c6c68
Faithfulness Alamu <vaguemail369@gmail.com>
2021-04-12 10:17:22 +0100

    Update DB interface definition
      - Receive an ID instead of a full URL as the mapping key
      - Allow error return in GetHTMLFor method

--- internal/pkg/db/db.go
+++ internal/pkg/db/db.go
@@ -2,6 +2,6 @@ package db
 
 // DB defines the API for any database implementation to be used in the mark2web service
 type DB interface {
-	Save(URL string, HTML []byte) error
-	GetHTMLFor(URL string) []byte
+	Save(ID string, HTML []byte) error
+	GetHTMLFor(ID string) ([]byte, error)
 }
