58b52c7ba8b3456936a9798b524393a4c6e8becc
Faithfulness Alamu <vaguemail369@gmail.com>
2021-04-13 08:49:55 +0100

    Implement Save DB method in FirebaseDB

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/thealamu/mark2web-service
 go 1.16
 
 require (
+	firebase.google.com/go/v4 v4.4.0 // indirect
 	github.com/gomarkdown/markdown v0.0.0-20210408062403-ad838ccf8cdd
 	github.com/sirupsen/logrus v1.8.1
 )
---
+++ internal/pkg/db/firebase.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"cloud.google.com/go/storage"
+	firebase "firebase.google.com/go/v4"
+)
+
+// FirebaseDB is a Firebase implementation of the DB interface
+type FirebaseDB struct {
+	bucket *storage.BucketHandle
+}
+
+func NewFirebaseDB() (*FirebaseDB, error) {
+	config := &firebase.Config{
+		StorageBucket: "mark2web.appspot.com",
+	}
+	app, err := firebase.NewApp(context.Background(), config)
+	if err != nil {
+		return nil, err
+	}
+	client, err := app.Storage(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	bucket, err := client.DefaultBucket()
+	if err != nil {
+		return nil, err
+	}
+	return &FirebaseDB{bucket}, nil
+}
+
+func (f *FirebaseDB) Save(ID string, HTML []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
+	defer cancel()
+
+	wc := f.bucket.Object(ID).NewWriter(ctx)
+	defer wc.Close()
+
+	_, err := wc.Write(HTML)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (f *FirebaseDB) GetHTMLFor(ID string) ([]byte, error) {
+	return nil, nil
+}
